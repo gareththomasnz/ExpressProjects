7c03ca8a81d5e75a447fc4265fcd777ed0342fc9
gareththomasnz <gareththomasnz@yahoo.com>
2017-01-17 15:24:28 +1300

    fourth commit

--- pcrepair/app.js
+++ pcrepair/app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var about = require('./routes/about');
+var services = require('./routes/services');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/about', about);
+app.use('/services', services);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
