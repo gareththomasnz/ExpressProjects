e889cecedacdc24af93fc3438f676564c8cb5cb7
gareththomasnz <gareththomasnz@yahoo.com>
2017-01-17 17:42:39 +1300

    sixth commit

--- pcrepair/routes/contact.js
+++ pcrepair/routes/contact.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
+/* https://documentup.com/andris9/nodemailer/ */
+router.post('/send', function(req, res){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'gmail.user@gmail.com',
+      pass: 'password'
+    }
+    });
+  
+ var mailOptions = {
+      from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
+      to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world ✔', // plaintext body
+      html: '<b>Hello world ✔</b>' // html body
+  };
+  
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+        res.redirect('/'):
+      }
+      console.log('Message sent: ' + info.response);
+        res.redirect('/'):
+  });
+
+  });
 
 module.exports = router;
