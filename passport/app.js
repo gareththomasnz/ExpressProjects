c82cf02ecd874fe2c1c54b38da225051d81724fc
gareththomasnz <gareththomasnz@yahoo.com>
2017-02-01 19:51:18 +1300

    sixteenth commit

--- passport/app.js
+++ passport/app.js
@@ -6,44 +6,28 @@ var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var flash = require('connect-flash');
-var session = require('express-session');
+var session  = require('express-session');
 var passport = require('passport');
 var mongoose = require('mongoose');
+var flash = require('connect-flash');
 
-var configDB = require('./config/db.js');
+var configDB = require('./config/db');
+require('./config/passport')(passport);
 mongoose.connect(configDB.url);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
-app.set('view engine', 'handlebars');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: 'secret',
   saveUninitialized: true,
-  resave: true
-  }));
+  resave: 'true'
+}));
 
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-});
+app.use(flash());
 
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.');
@@ -61,8 +45,31 @@ app.use(expressValidator({
   }
 }));
 
-app.use('/', index);
-app.use('/users', users);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//makes a global variable to access the flash messages within the view template
+
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -71,15 +78,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
---
+++ passport/config/passport.js
@@ -0,0 +1,88 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+var bcrypt = require('bcryptjs');
+
+
+module.exports = function(passport) {
+	passport.serializeUser(function(user, done) {
+		done(null, user._id);
+	});
+
+	passport.deserializeUser(function(id, done) {
+		User.getUserById(id, function(err, user) {
+			done(err, user);
+		});
+	});
+
+	//Register
+	passport.use('local-register', new LocalStrategy({
+		passReqToCallback: true
+	}, function(req, username, password, done) {
+		findOrCreateUser = function() {
+			//find a user with this username
+			User.findOne({
+				username: username
+			}, function(err, user) {
+				if (err) {
+					console.log("Error" + err);
+					return done(null, false, {message: err});
+				}
+
+				if (user) {
+					console.log('User already exists');
+					return done(null, false, {message: 'user already exists'});
+				} else {
+					var newUser = new User();
+					newUser.username = username;
+					newUser.password = createHash(password);
+					newUser.email = req.param('email');
+					newUser.name = req.param('name');
+					newUser.join_date = new Date();
+
+					User.addUser(newUser, function(err, user) {
+						if (err) {
+							console.log('Error: ' + err);
+							throw err;
+						} else {
+							req.flash('success', 'Your are now registered and logged in');
+							return done(null, newUser);
+						}
+					});
+				}
+
+			});
+		};
+		process.nextTick(findOrCreateUser);
+	}));
+
+	passport.use('local-login', new LocalStrategy({
+		passReqToCallback: true
+	}, function(req, username, password, done){
+		User.getUserByUsername(username, function(err, user){
+			if(err){
+				console.log('Error' + err);
+				return done(err);
+			}
+			
+			if(!user){
+				console.log('Error : user not found');
+				return done(null, false, req.flash('error', 'User not found'));
+			}
+
+			if(!isValidPassword(user, password)){
+				return done(null, false, req.flash('error', 'Invalid password'));
+			}
+
+			
+			return done(null, user, req.flash('success', 'You are now logged in'));
+		});
+	}));
+
+	var createHash = function(password) {
+		return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+	};
+
+	var isValidPassword = function(user, password){
+		return bcrypt.compareSync(password, user.password);
+	};
+};
